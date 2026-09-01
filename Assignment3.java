3ac78faab2da029d05b4f87e0fd7e566bd1609dd
Aaron Brycet <aaronbrycet@gmail.com>
2026-09-01 23:36:53 +0800

    Add Assignment 3

---
+++ Assignment3.java
@@ -0,0 +1,20 @@
+
+public class Assignment3 {
+    
+    public static void main(String[] args) {
+        String firstname = "Leo Gabriel";
+        String lastname = "Villanueva";
+        String address = "Urdaneta City";
+        String mobileNumber = "0933889111";
+        int age = 20;
+        double gwa = 98.23;
+        
+        System.out.println("Firstname: " + firstname);
+        System.out.println("Lastname: " + lastname);
+        System.out.println("Address: " + address);
+        System.out.println("Mobile Number: " + mobileNumber);
+        System.out.println("Age: " + age);
+        System.out.println("GWA: " + gwa);
+    }
+}       
+    
