a0437cc304dfeff89c51c28110aa2ecf4956457f
Aaron Brycet <aaronbrycet@gmail.com>
2026-09-02 00:56:02 +0800

    Update Assignment 3

--- Assignment3.java
+++ Assignment3.java
@@ -1,20 +1,20 @@
 
-public class Assignment3 {
+public class Assignment3 { //Declaring a public class named Assignment3.java
     
-    public static void main(String[] args) {
-        String firstname = "Leo Gabriel";
-        String lastname = "Villanueva";
-        String address = "Urdaneta City";
-        String mobileNumber = "0933889111";
-        int age = 20;
-        double gwa = 98.23;
+    public static void main(String[] args) { //Declaring the main method
+        String firstname = "Aaron Bryce"; //Declaring String variable for the first name
+        String lastname = "Ticman"; //Declaring String variable for the lastname
+        String address = "Urdaneta City"; //Declaring String variable for the address
+        String mobileNumber = "09672677647"; //Declaring String variable for the mobilenumber
+        int age = 18; //Declaring an int variable for the age
+        double gwa = 93.00; //Declaring double variable for the gwa
         
-        System.out.println("Firstname: " + firstname);
-        System.out.println("Lastname: " + lastname);
-        System.out.println("Address: " + address);
-        System.out.println("Mobile Number: " + mobileNumber);
-        System.out.println("Age: " + age);
-        System.out.println("GWA: " + gwa);
-    }
-}       
+        System.out.println("Firstname: " + firstname); //Displays firstname
+        System.out.println("Lastname: " + lastname); //Displays lastname
+        System.out.println("Address: " + address); //Displays address
+        System.out.println("Mobile Number: " + mobileNumber); //Displays mobilenumber
+        System.out.println("Age: " + age); //Displays age
+        System.out.println("GWA: " + gwa); //Displays GWA
+    } //Closing the main method
+}   //Closing the Assignment3.java
     
